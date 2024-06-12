4009bc12c885b3787b9be52a22edae5e7a9a938c
shl622 <brian971002@gmail.com>
2024-06-12 07:30:09 -0400

    refactor: updates input and button components in SMS verification

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,5 +1,5 @@
-import FormInput from "@/components/form-input"
-import FormButton from "@/components/form-btn"
+import Input from "@/components/input"
+import Button from "@/components/button"
 
 export default function createAccount(){
     return(
@@ -9,21 +9,23 @@ export default function createAccount(){
                 <h2 className="text-xl">Verify your phone number</h2>
             </div>
             <form className="flex flex-col gap-3">
-                <FormInput
+                <Input
+                    name="phoneNumber"
                     type="number"
                     placeholder="Phone number"
                     required
                     errors={[]}
                 />
-                <FormInput
+                <Input
+                    name="verificationNumber"
                     type="number"
                     placeholder="Verification Code"
                     required
                     errors={[]}
                 />
-                <FormButton
-                    loading={false}
+                <Button
                     text="Verify"
+                    loadMsg="Verifying..."
                 />
             </form>
         </div>
