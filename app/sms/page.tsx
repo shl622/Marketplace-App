cd61f2c57683d8cf54627c38ff6b978f9204a4ec
shl622 <brian971002@gmail.com>
2024-06-12 08:50:09 -0400

    feat: adds mobile verification page / build: adds validatorjs

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,31 +5,41 @@ import Button from "@/components/button"
 import { useFormState } from "react-dom"
 import { smsVerification } from "./actions"
 
-export default function createAccount(){
-    const [state,dispatch] = useFormState(smsVerification, null)
-    return(
+const initialState = {
+    token: false,
+    error: undefined
+}
+
+export default function smsLogin() {
+    const [state, dispatch] = useFormState(smsVerification, initialState)
+    return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">SMS Login</h1>
                 <h2 className="text-xl">Verify your phone number</h2>
             </div>
             <form action={dispatch} className="flex flex-col gap-3">
-                <Input
-                    name="phoneNumber"
-                    type="number"
-                    placeholder="Phone number"
-                    required
-                />
-                <Input
-                    name="token"
-                    type="number"
-                    placeholder="Verification Code"
-                    required
-                />
-                <Button
-                    text="Verify"
-                    loadMsg="Verifying..."
-                />
+                {state.token ? (
+                    <Input
+                        key={1}
+                        name="token"
+                        type="number"
+                        placeholder="Verification code"
+                        required
+                        min={100000}
+                        max={999999}
+                    />
+                ) : (
+                    <Input
+                        key={2}
+                        name="phoneNumber"
+                        type="text"
+                        placeholder="Phone number"
+                        required
+                        errors={state.error?.formErrors}
+                    />
+                )}
+                <Button text={state.token ? "Verify Token" : "Send Verification SMS"} loadMsg="verifying..." />
             </form>
         </div>
     )
