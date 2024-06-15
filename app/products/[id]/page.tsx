65ff8d6f87023c6ae4df4f98448824d415217c41
shl622 <brian971002@gmail.com>
2024-06-15 08:53:38 -0400

    feat: adds loading skeleton for product description loading

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,10 @@
-export default function ProductDetail(){
-    return <span>Product detail!</span>
+async function getProduct(){
+    await new Promise(resolve=> setTimeout(resolve,10000))
+}
+
+export default async function ProductDetail({params:
+    {id}
+}:{params:{id:string}}){
+    const product = await getProduct()
+    return <span>Product detail of the product {id}!</span>
 }
