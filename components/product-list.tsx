657742dd9dfd841481c525dd710ca69dd44995f7
shl622 <brian971002@gmail.com>
2024-06-16 08:48:58 -0400

    feat: infinite scrolling imitated using hooks so new products load when user scrolls past the rendered products

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ListProduct from "./list-product";
 import { initialProducts } from "@/app/(tabs)/products/page";
 import { getMoreProducts } from "@/app/(tabs)/products/action";
@@ -9,38 +9,59 @@ interface ProductsListProps {
     initialProducts: initialProducts
 }
 
-
 export default function ProductList({ initialProducts }: ProductsListProps) {
     const [products, setProducts] = useState(initialProducts)
     const [isLoading, setIsLoading] = useState(false)
     const [page, setPage] = useState(0)
     const [isLastPage, setIsLastPage] = useState(false)
-    const onLoadMoreClick = async()=>{
-        setIsLoading(true)
-        const newProducts = await getMoreProducts(page+1)
-        if (newProducts.length !== 0){
-            setPage((prev)=>prev + 1)
-            setProducts((prev)=>[...prev,...newProducts])
-        }else{
-            setIsLastPage(true)
+    const trigger = useRef<HTMLSpanElement>(null)
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            async (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+                const element = entries[0]
+                if (element.isIntersecting && trigger.current) {
+                    observer.unobserve(trigger.current)
+                    setIsLoading(true)
+                    const newProducts = await getMoreProducts(page + 1)
+                    if (newProducts.length !== 0) {
+                        setPage((prev) => prev + 1)
+                        setProducts((prev) => [...prev, ...newProducts])
+                    } else {
+                        setIsLastPage(true)
+                    }
+                    setIsLoading(false)
+                }
+            },{
+                threshold:1.0,
+                rootMargin: "0px 0px -100px 0px"
+            }
+        )
+        //observe the button
+        if (trigger.current) {
+            observer.observe(trigger.current)
         }
-        setIsLoading(false)
-    }
+        //clean up 
+        return () => {
+            observer.disconnect()
+        }
+    }, [page])
     return (
         <div className="p-5 flex flex-col gap-5">
-            {products.map((product)=>(
+            {products.map((product) => (
                 <ListProduct key={product.id} {...product} />
             ))}
-            {isLastPage ? ("No more items"):(
-                <button onClick={onLoadMoreClick}
-                disabled={isLoading}
-                className="text-sm font-semibold
+            {!isLastPage?(
+                <span
+                ref={trigger}
+                style={{
+                    marginTop:`${page+1*900}vh`,
+                }}
+                className="mb-96 text-sm font-semibold
                 bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md
                 hover:opacity-90 active:scale-95">
-                    {isLoading ? "loading..." : "Show More"}
-                </button>
-            )}
-            
+                {isLoading ? "loading..." : "Show More"}
+            </span>
+            ): null}
         </div>
     )
 }
